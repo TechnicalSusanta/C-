e886096d9c752d67d3e3a03d20a5b05a700c7e0c
Sanchita Saha <166200570+iamsanchita@users.noreply.github.com>
2024-08-11 11:15:06 +0530

    Update sum_of_given_series.cpp
    shorten the code

--- sum_of_given_series.cpp
+++ sum_of_given_series.cpp
@@ -1,23 +1,5 @@
 #include<iostream>
 using namespace std;
-int factorial(int num){    
-    int fact= 1;
-    for(int i=1;i<=num;i++){
-        fact*=i;
-    }
-    return fact;
-}
-int sumoffact(int num){
-    int sum=0;
-    for(int j=1;j<num;j++){
-        sum+=factorial(j);
-    }
-    return sum;
-}
-    int main(){
-        int num;
-        cout<<"Enter the number:";
-        cin>>num;
-        cout<<sumoffact(num+1)<<endl;
-        return 0;
-    }
+int fact(int n){if(n == 0 || n == 1) return 1;else return n*fact(n-1);}
+int sum(int x,int sum = 0){for(int i=1;i<x;i++) sum+=fact(i);}
+int main(){cout<<sum(3);}
